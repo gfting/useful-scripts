cd8e1c7ba66f23776b8454f49dcd4e9984921154
Gabriel Ting <gabrielfting@gmail.com>
2019-12-17 23:34:37 -0600

    Properly async refactoring

---
+++ README.md
@@ -0,0 +1,7 @@
+# useful-scripts
+
+A collection of useful scripts for various purposes.
+
+## class-log.js
+
+This script is to go through YES (Vanderbilt's enrollment system) and get a listing of all the CS classes and their times so we can run marketing in classrooms for VandyHacks more easily.
--- class-log-script.js
+++ class-log-script.js
@@ -1,75 +1,102 @@
-// opens adv link
-document.getElementById("advancedSearchLink").click();
+// sleep function for async execution
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-// selects CS as subject area
-document.getElementById("subjAreaMultiSelectOption57").checked = true;
+// through advanced menu, searches for just CS courses
+function searchCS() {
+	// selects CS as subject area
+	document.getElementById("subjAreaMultiSelectOption57").checked = true;
 
-// sends search request
-document.getElementById("advancedSearchClassesSubmit-button").click();
-
-// checks if there are any results yet, waits until it's loaded
-while (document.getElementById("searchClassSectionsResults").innerHTML === "") {
-	window.setTimeout({}, 1000); // waits a second until it's loaded again
+	// sends search request
+	document.getElementById("advancedSearchClassesSubmit-button").click();
 }
 
 // creates a JSON object to hold on to the class data for us
 let classesObj = {};
 
-// gets all classes on the page
-let classes = document.getElementsByClassName("classTable");
+function loadClasses() {
+	// gets all classes on the page
+	let classes = document.getElementsByClassName("classTable");
 
-// iterates through all of the classes we see on the page
-for (let i = 0; i < classes.length; ++i) {
-	// create a new object linked to the class name
-	let classObj = {};
+	// iterates through all of the classes we see on the page
+	for (let i = 0; i < classes.length; ++i) {
+		// create a new object linked to the class name
+		let classObj = {};
 
-	let classInfo = classes[i];
+		let classInfo = classes[i];
 
-	// gets the class number
-	classObj["classNumber"] = classInfo.getElementsByClassName(
-		"classAbbreviation"
-	).innerText;
+		// gets the class number
+		classObj["classNumber"] = classInfo
+			.getElementsByClassName("classAbbreviation")[0]
+			.innerText.trim();
 
-	let className = classInfo.getElementsByClassName("classDescription")
-		.innerText;
+		let className = classInfo
+			.getElementsByClassName("classDescription")[0]
+			.innerText.trim();
 
-	// gets the class name
-	classObj["className"] = className;
+		// gets the class name
+		classObj["className"] = className;
 
-	// get all class listings; each of these are rows
-	let classListings = classInfo.getElementsByClassName("classRow");
+		// get all class listings; each of these are rows
+		let classListings = classInfo.getElementsByClassName("classRow");
 
-	// iterate through each class listing
-	for (let j = 0; j < classListings.length; ++j) {
-		let listingObj = {};
-		// add the time
-		listingObj["days"] = classListings[j]
-			.getElementsByClassName("classMeetingDays")
-			.innerText.strip();
-		listingObj["time"] = classListings[j]
-			.getElementsByClassName("classMeetingTimes")
-			.innerText.strip();
+		console.log(classObj);
 
-		// add the location
-		listingObj["location"] = classListings[j]
-			.getElementsByClassName("classBuilding")
-			.innerText.strip();
+		// iterate through each class listing
+		for (let j = 0; j < classListings.length; ++j) {
+			let listingObj = {};
+			// add the time
+			listingObj["days"] = classListings[j]
+				.getElementsByClassName("classMeetingDays")[0]
+				.innerText.trim();
+			listingObj["time"] = classListings[j]
+				.getElementsByClassName("classMeetingTimes")[0]
+				.innerText.trim();
 
-		// add each professor's name
-		listingObj["instructor"] = classListings[j]
-			.getElementsByClassName("classInstructor")
-			.innerText.strip();
-		// pushes on the section with this listing
-		classObj[`section${j}`] = listingObj;
+			// add the location
+			listingObj["location"] = classListings[j]
+				.getElementsByClassName("classBuilding")[0]
+				.innerText.trim();
+
+			// add each professor's name
+			listingObj["instructor"] = classListings[j]
+				.getElementsByClassName("classInstructor")[0]
+				.innerText.trim();
+			// pushes on the section with this listing
+			classObj[`section${j}`] = listingObj;
+		}
+
+		// ends by pushing on the classObj
+		classesObj[className] = classObj;
 	}
 
-	// ends by pushing on the classObj
-	classesObj[className] = classObj;
+	console.log(classesObj);
+
+	// go to the next page
+	// must find the second page of the paginator results
+	document.getElementById("paginator");
+	// first iteration should get the 4th child, or the a link to the next page; afterwards, should get the 6th child
 }
 
-console.log(classesObj);
+async function driver() {
+	// opens adv search list
+	document.getElementById("advancedSearchLink").click();
+	await sleep(2000);
+	searchCS();
 
-// go to the next page
-// must find the second page of the paginator results
-document.getElementById("paginator");
-// first iteration should get the 4th child, or the a link to the next page; afterwards, should get the 6th child
+	// checks if there are any results yet, waits until it's loaded
+
+	while (
+		document.getElementById("searchClassSectionsResults")
+			.childElementCount === 1
+	) {
+		console.log("loading");
+		await sleep(1000); // waits a second until it's loaded again
+	}
+
+	// loads the classes after
+	loadClasses();
+}
+
+driver();
