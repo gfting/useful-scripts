30e95ff01bb342e38492eb0621696311b5cc06b6
Gabriel Ting <gabrielfting@gmail.com>
2019-12-17 21:44:04 -0600

    Attempts to wait for class listing load

--- class-log-script.js
+++ class-log-script.js
@@ -1,1 +1,10 @@
-document.getElementById("advancedSearchLink").click();
+// selects CS as subject area
+document.getElementById("subjAreaMultiSelectOption57").checked = true;
+
+// sends search request
+document.getElementById("advancedSearchClassesSubmit-button").click();
+
+// checks if there are any results yet, waits until it's loaded
+while (document.getElementById("searchClassSectionsResults").innerHTML === "") {
+	window.setTimeout({}, 1000); // waits a second until it's loaded again
+}
--- class-log.js
+++ class-log.js
@@ -2,6 +2,6 @@ javascript: (function() {
 	let script = document.createElement("script");
 	script.type = "text/javascript";
 	script.src =
-		"https://rawgit.com/sxlijin/assorted-code-snippets/master/random-vandy-stuff/vandyClassRegistration.js";
+		"https://rawcdn.githack.com/gfting/useful-scripts/c0da3a7bf5f30baa80ba0ca8c56b2c12c4d409f2/class-log-script.js";
 	document.body.appendChild(script);
 })();
