c0da3a7bf5f30baa80ba0ca8c56b2c12c4d409f2
Gabriel Ting <gabrielfting@gmail.com>
2019-12-17 21:31:16 -0600

    Initial script addition

---
+++ class-log-script.js
@@ -0,0 +1,1 @@
+document.getElementById("advancedSearchLink").click();
---
+++ class-log.js
@@ -0,0 +1,7 @@
+javascript: (function() {
+	let script = document.createElement("script");
+	script.type = "text/javascript";
+	script.src =
+		"https://rawgit.com/sxlijin/assorted-code-snippets/master/random-vandy-stuff/vandyClassRegistration.js";
+	document.body.appendChild(script);
+})();
